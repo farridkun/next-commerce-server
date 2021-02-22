50ba7ae3dfcf0282505c859244ad189ee0fbb1ff
farridkun <farridkuntoro@mahasiswa.unikom.ac.id>
2021-02-22 19:50:46 +0700

    FIX version of Strapi 3.5.0 update bug and minor

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '395c302280e823dfe3ce836ef0f011dc'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7da623c2-5f1e-4a85-ae94-a663548a6997'
+};
