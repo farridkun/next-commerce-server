438d3eea92c215a7125a6faf0e4e60e655544b56
farridkun <farridkuntoro@mahasiswa.unikom.ac.id>
2021-02-22 20:05:27 +0700

    Newer revision bug on condition plugin

--- config/plugins.js
+++ config/plugins.js
@@ -1,15 +1,23 @@
 module.exports = ({env}) => {
-    return {
-        upload: {
-            provider: 'aws-s3',
-            providerOptions: {
-                accessKeyId: env('AWS_ACCESS_KEY_ID'),
-                secretAccessKEY: env('AWS_ACCESS_SECRET'),
-                region: env('AWS_REGION'),
-                params: {
-                    Bucket: env('AWS_BUCKET')
+
+    if (env('NODE_ENV') === 'production') {
+        return {
+            upload: {
+                provider: 'aws-s3',
+                providerOptions: {
+                    accessKeyId: env('AWS_ACCESS_KEY_ID'),
+                    secretAccessKEY: env('AWS_ACCESS_SECRET'),
+                    region: env('AWS_REGION'),
+                    params: {
+                        Bucket: env('AWS_BUCKET')
+                    }
                 }
             }
         }
     }
+
+    return {
+        
+    }
+
 }
